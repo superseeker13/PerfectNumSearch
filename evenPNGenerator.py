d61fe68b0527cacf373976a3b18e1410581c1634
Edward <cardseller2@gmail.com>
2019-09-04 20:15:13 -0400

    Output change

--- evenPNGenerator.py
+++ evenPNGenerator.py
@@ -1,4 +1,6 @@
-from csv import QUOTE_ALL, writer
+from csv import QUOTE_NONE, writer
+import concurrent
+import datetime
 
 
 def generateEvenPNums(num) -> list:
@@ -10,12 +12,13 @@ def generateEvenPNumsInRange(rage) -> list:
     # Increases performace for large values
     append = perfectList.append
     for i in rage:
-        print('i = ' + str(i))
         p = (2**i) - 1
+        date = datetime.datetime.now()
         if isPrime(p):
             q = 2**(i-1)
-            print('p, q = ', p, q)
-            print(str(p*q) + ' is a perfect number.')
+            print('Prime check time: ', datetime.datetime.now() - date)
+            print('i, p, q = ', i, p, q)
+            print(str(p*q) + ' is a perfect number.\n')
             append(int(p*q))
     listToCsv(perfectList)
     return perfectList
@@ -27,6 +30,7 @@ def isPrime(num) -> bool:
     elif num % 2 == 0 or num % 3 == 0 or num <= 1:
         return False
     else:
+        # executor = concurrent.futures.ProcessPoolExecutor(2)
         gen = (i for i in range(7, (int(num / 2) + 1), 2) if i % 3 != 0)
         for i in gen:
             if num % i == 0:
@@ -36,5 +40,5 @@ def isPrime(num) -> bool:
 
 def listToCsv(myList, fileName='perfectNums.csv'):
     with open(fileName, 'w', newline='') as myfile:
-        wr = writer(myfile, quoting=QUOTE_ALL)
+        wr = writer(myfile, quoting=QUOTE_NONE)
         wr.writerow(myList)
